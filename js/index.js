cba05d4a51e1b8b55056b93be5120ca630830964
Hrithik Karmakar <74124287+hrithik0@users.noreply.github.com>
2022-08-22 17:21:02 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+document.querySelector('button').addEventListener('click', ()=>{
+    document.querySelector('.field').style.display = 'none';
+    document.querySelector('.greet').style.display = 'initial';
+
+})
+
+function change(){
+    document.activeElement.ariaValueNow
+}
